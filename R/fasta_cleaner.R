5db1a88b01a00fe9b34ed42165707a97d4a62cf7
Nathan Brouwer <brouwern@gmail.com>
2021-05-11 11:31:09 -0400

    add sliding window draft

---
+++ R/fasta_cleaner.R
@@ -0,0 +1,41 @@
+#' Function to convert a FASTA file stored as an object into a vector
+#'
+#' @param fasta_object R object with FASTA character data to clean
+#' @param parse Parse data to character vector?
+#'
+#' @examples
+#'
+#' header. <- ">NC_001477.1 Dengue virus 1, complete genome"
+#' fasta_cleaner(dengueseq_fasta, parse = TRUE)
+#'
+#' @export
+
+fasta_cleaner <- function(fasta_object, parse = TRUE){
+
+  #header <- gsub("\\|","",header)
+  #header <- gsub("\\+","",header)
+  #header <- gsub("\\=","",header)
+  #header <- gsub("\\:","",header)
+  #header <- gsub("\\;","",header)
+
+  #fasta_object <- gsub("\\|","",fasta_object)
+  #fasta_object <- gsub("\\+","",fasta_object)
+  #fasta_object <- gsub("\\=","",fasta_object)
+  #fasta_object <- gsub("\\:","",fasta_object)
+  #fasta_object <- gsub("\\;","",fasta_object)
+
+
+  #fasta_object <- gsub(header, "", fasta_object)
+
+  fasta_object <- sub("^(>)(.*?)(\\n)(.*)(\\n\\n)","\\4",fasta_object)
+  fasta_object <- gsub("\n", "", fasta_object)
+
+  if(parse == TRUE){
+    fasta_object <- stringr::str_split(fasta_object,
+                                       pattern = "",
+                                       simplify = FALSE)
+  }
+
+
+  return(fasta_object[[1]])
+}
